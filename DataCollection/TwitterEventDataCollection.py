25277ee46dc003e8b5a593797e554f463cfeecbb
dxmahata <debanjanbhucs@gmail.com>
2015-05-09 14:57:29 -0500

    updated methods in event data collection

--- DataCollection/TwitterEventDataCollection.py
+++ DataCollection/TwitterEventDataCollection.py
@@ -24,34 +24,47 @@ collection = db[""]
 
 
 class MyStreamer(TwythonStreamer): 
+    def extractTweetInfo(self,tweet):
+        tweetInfoExtractObj = tp.TweetInfoExtract(tweet)
+        tweetFeatures = tweetInfoExtractObj.features
+        return tweetFeatures
+        
+    
+    def assignInfoScore(self,tweet):
+        tweetObj = tweetProcess.TweetProcess(tweet)
+        tweetClassifyObj = tweetClassify.TweetScore()
+        tweetClassifyObj.setTweetInstance(tweetObj.getDataInstanceForClassification())
+        tweetInfoScore = tweetClassifyObj.getTweetPosScore()
+        return tweetInfoScore
+ 
     
     def on_success(self, data): 
         if data["lang"] == "en":
-            tweet = data            
-            tweetObj = tweetProcess.TweetProcess(tweet)
-            tweetClassifyObj = tweetClassify.TweetScore()
-            tweetClassifyObj.setTweetInstance(tweetObj.getDataInstanceForClassification())
-            tweetInfoScore = tweetClassifyObj.getTweetPosScore()
-            if tweetInfoScore > 0.9:
-                print tweet["text"].encode("utf-8")
-                print tweetInfoScore
-            collection.insert(data)
+            dataDict = {"rawData":None,"extractedFeatures":None,"initInfoScore":0.0}
+            tweet = data 
+            infoScore = self.assignInfoScore(tweet) 
+            tweetFeatures = self.extractTweetInfo(tweet) 
+            dataDict["rawData"] = data
+            dataDict["extractedFeatures"] = tweetFeatures
+            dataDict["initInfoScore"] = infoScore
+       
+            collection.insert(dataDict)
             # Want to disconnect after the first result? 
             # self.disconnect() 
  
  
     def on_error(self, status_code, data): 
         sleep(randint(1,60))
-        keys = keyList[randint(0,9)]
+        keys = keyList[randint(0,12)]
         stream = MyStreamer(keys["APP_KEY"],keys["APP_SECRET"],keys["OAUTH_TOKEN"],keys["OAUTH_TOKEN_SECRET"])
-        stream.statuses.filter(track="#sxsw")
+        stream.statuses.filter(track="")
         
 ## Requires Authentication as of Twitter API v1.1 
 while True:
     try:
         keys = keyList[randint(0,12)]
         stream = MyStreamer(keys["APP_KEY"],keys["APP_SECRET"],keys["OAUTH_TOKEN"],keys["OAUTH_TOKEN_SECRET"]) 
-        stream.statuses.filter(track='#sxsw')
+        stream.statuses.filter(track='')
     except:
         continue
 
